a5f354f312d304b34f7083a1d2496abc4cce97fe
mino-0 <qawsedrfnnn@gmail.com>
2021-09-13 23:38:26 +0900

    Study Security & API ~580p

--- src/main/java/com/mino/club/config/SecurityConfig.java
+++ src/main/java/com/mino/club/config/SecurityConfig.java
@@ -1,35 +1,49 @@
 package com.mino.club.config;
 
+import com.mino.club.security.filter.ApiCheckFilter;
 import com.mino.club.security.handler.ClubLoginSuccessHandler;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+/*        http.authorizeRequests()
                 .antMatchers("/sample/all").permitAll()
-                .antMatchers("/sample/member").hasAnyRole("USER");
+                .antMatchers("/sample/member").hasAnyRole("USER");*/
+
         http.formLogin(); //인가/인증 문제시 로그인 화면
         http.csrf().disable(); //CSRF 토큰 발행하지 않게 설정
-
+        http.logout();
         http.oauth2Login().successHandler(successHandler());
+        http.rememberMe().tokenValiditySeconds(60*60*24*7)
+                .userDetailsService(userDetailsService()); //7days
+        http.addFilterBefore(apiCheckFilter(),
+                UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
     public ClubLoginSuccessHandler successHandler() {
         return new ClubLoginSuccessHandler(passwordEncoder());
+    }
+
+    @Bean
+    public ApiCheckFilter apiCheckFilter() {
+        return new ApiCheckFilter("/notes/**/*");
     }
 /*    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
--- src/main/java/com/mino/club/controller/SampleController.java
+++ src/main/java/com/mino/club/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.mino.club.controller;
 
 import com.mino.club.security.dto.ClubAuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Log4j2
 @RequestMapping("/sample/")
 public class SampleController {
+    @PreAuthorize("permitAll()")
     @GetMapping("/all")
     public void exAll() {
         log.info("exAll......");
@@ -24,8 +26,18 @@ public class SampleController {
         log.info(clubAuthMemberDTO);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public void exAdmin() {
         log.info("exAdmin......");
+    }
+
+    @PreAuthorize("#clubAuthMember != null && #clubAuthMember.username eq \"user95@mino.org\"")
+    @GetMapping("/exonly")
+    public String exMemberOnly(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember) {
+        log.info("exMemberOnly.................");
+        log.info(clubAuthMember);
+
+        return "/sample/admin";
     }
 }
